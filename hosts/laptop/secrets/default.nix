dcd38e56d73efefbdd1da0a97e46558e59829f9c
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-03-24 16:07:27 +0100

    secrets
    Former-commit-id: 0c111415aa1d9590ae0960e1fda59158e235dd11 [formerly d08e3b73b14b3e5263c7dda90423ec703ddd23fc]
    Former-commit-id: 9943e163bbc1fd629b664e7edc899c81f31dd14f

---
+++ hosts/laptop/secrets/default.nix
@@ -0,0 +1,38 @@
+{ pkgs, inputs, ... }: {
+  imports = [ inputs.sops-nix.homeManagerModules.sops ];
+
+  sops = {
+    age.keyFile = "/home/dilounix/.config/sops/age/keys.txt";
+    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFormat = "yaml";
+    secrets = {
+      ssh-config = { path = "/home/dilounix/.ssh/config"; };
+      github-key = { path = "/home/dilounix/.ssh/github"; };
+      weather-key = { path = "/home/dilounix/.weather.json"; };
+      #gitlab-key = { path = "/home/dilounix/.ssh/gitlab"; };
+      #jack-key = { path = "/home/dilounix/.ssh/jack"; };
+      #pia = { path = "/home/dilounix/.config/pia/pia.ovpn"; };
+
+    };
+  };
+
+  home.file.".config/nixos/.sops.yaml".text = ''
+    keys:
+      - &primary age187l3764s2kd49mn6tmagjgmke0t5ak2lgwqnhxayfjt7dqu8ycesa68u73
+    creation_rules:
+      - path_regex: hosts/laptop/secrets/secrets.yaml$
+        key_groups:
+          - age:
+            - *primary
+      - path_regex: hosts/server/secrets/secrets.yaml$
+        key_groups:
+          - age:
+            - *primary
+  '';
+
+  systemd.user.services.mbsync.Unit.After = [ "sops-nix.service" ];
+  home.packages = with pkgs; [ sops age ];
+
+  wayland.windowManager.hyprland.settings.exec-once =
+    [ "systemctl --user start sops-nix" ];
+}
