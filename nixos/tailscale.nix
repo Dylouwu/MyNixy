47f03055b63c7ce0a94e0e03cbf5cf1a8f2c533a
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-03-24 18:17:46 +0100

    tailscale is rdy

--- nixos/tailscale.nix
+++ nixos/tailscale.nix
@@ -1,1 +1,18 @@
-{ services.tailscale = { enable = true; }; }
+
+{ config, ... }: {
+  security.sudo.extraRules = [{
+    users = [ config.var.username ];
+    commands = [
+      {
+        command = "/etc/profiles/per-user/${config.var.username}/bin/tailscale";
+        options = [ "NOPASSWD" ];
+      }
+      {
+        command = "/run/current-system/sw/bin/tailscale";
+        options = [ "NOPASSWD" ];
+      }
+    ];
+  }];
+  services.tailscale = { enable = true; };
+}
+
