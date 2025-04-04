730cf282f5a7a3820cc7d323db98ae26f64094dc
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-04 11:57:19 +0200

    zen locked to 1.10 as there are framerate issues with last version
    Former-commit-id: 7c3e08278dfb3667dd060c18b44e790325e34173 [formerly ace13dd38bd768039610e547e7ddd0ef00423da4]
    Former-commit-id: a53e04b18762090bd07c167793d879cbb32b86d7

--- flake.nix
+++ flake.nix
@@ -12,7 +12,8 @@
     hyprpanel.url = "github:Jas-SinghFSU/HyprPanel";
     stylix.url = "github:danth/stylix";
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser.url =
+      "github:0xc000022070/zen-browser-flake/3c4f98e9504d3f94bbd303d428162665a0ade8d6";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
