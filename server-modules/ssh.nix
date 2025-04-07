a218160daadc4668ecfad46e288ed8f59d68a6e2
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-08 00:14:19 +0200

    Update ssh.nix
    Former-commit-id: 2ec63d6912fcc9a1b89c2319407a0a2ad4fc0e84 [formerly 2907a45889334b346d159c3d9102f44417c20bed]
    Former-commit-id: 8389545ba74b29b2cacb9537456af95372dfe57f

--- server-modules/ssh.nix
+++ server-modules/ssh.nix
@@ -13,6 +13,7 @@
   users.users."${config.var.username}" = {
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIBF4SKCHAyjQddBkTJhtK36ENayvsPKMmiALCAxzX6D dilounix@nixy"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCs80tggMfcG0dnnTMy/v0dPzEzM9jfntMlfkZrtmtjwz6+xWspDOd09hxGgpbBip7nh81nRK0HL/YDj8ycwq4Nq4ODyKmKjXXaNfWB32GEFSXoDdrMH1Oya00x6KoWH7AkHD/vE1hxcj2yLvCy3FkExGuL1pVGfTHTOvjj9VioJdcWcstsw7lSDXJ5ZzkujlpmLsRy53bwaPAeQXT9wfwTdfGmDnVsQtMthK8/eG7GSuIgJeiMx6pJpyYZmTxZapBlF7KPGQvg9nb4Aagc4ShjOFbdNowyVtfqSMy4dl3GFnVRE3r4PnVsq+c0E0v+OrwAnTnujtQbEYgL4OowSh0C/2XwvNgJvZvBhLd8EqURAF1UtNJzvGlpwa5iKwH183a85F8fu2pJmx39qwytqv0QozB2WrHR85pi5341j3C3NK8LNVAwvGheNmnC0OrVYjdYYljLNjGw1HO1fTyEGw3X2ypAHyrkR+89WtC0MN6C9L03FZqO0v/mMNifEwTV2Rk= dilou@PC_de_dilou"
     ];
   };
 }
