86b089c7a89e8bd9276696f5bffb24a0142c1de1
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-11 09:51:54 +0200

    cleaner

--- server-modules/nginx.nix
+++ server-modules/nginx.nix
@@ -1,4 +1,6 @@
-{ config, ... }: {
+{ config, ... }:
+let domain = "dilou.me";
+in {
   services.nginx = {
     enable = true;
 
@@ -7,8 +9,8 @@
       locations."/" = { return = 444; };
     };
 
-    virtualHosts."*.dilou.me" = {
-      useACMEHost = "dilou.me";
+    virtualHosts."*.${domain}" = {
+      useACMEHost = domain;
       forceSSL = true;
       locations."/" = { return = 444; };
     };
@@ -19,13 +21,12 @@
     defaults.email = config.var.git.email;
   };
 
-  security.acme.certs."dilou.me" = {
-    domain = "dilou.me";
-    extraDomainNames = [ "*.dilou.me" ];
+  security.acme.certs."${domain}" = {
+    domain = domain;
+    extraDomainNames = [ "*.${domain}" ];
     group = "nginx";
     dnsProvider = "cloudflare";
     dnsPropagationCheck = true;
     credentialsFile = config.sops.secrets.cloudflare-dns-token.path;
   };
-
 }
