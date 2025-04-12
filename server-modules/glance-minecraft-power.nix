01d58509ac3d9e51d9f23f0aa6120eaf435acc7d
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-12 10:48:17 +0200

    flake and server module updated
    Former-commit-id: d081eacb93db778284680b91bd3ab55147124b97 [formerly 09fb38f92e2457a62e4a9ca17023c6bff19630b7]
    Former-commit-id: 043d0c6ba9498d2226aba05d8187b7b85220081a

--- server-modules/glance-minecraft-power.nix
+++ server-modules/glance-minecraft-power.nix
@@ -1,6 +1,7 @@
 { config, ... }: {
   services.glance-minecraft-power = {
     enable = true;
+    minecraftServerName = "paradisum";
     apiKeyPath = config.sops.secrets.minecraft-api-key.path;
   };
 }
