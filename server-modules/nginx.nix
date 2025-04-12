d153467b34ddf8ccf4a73c0c8badd357c24e0d0a
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-12 11:55:08 +0200

    clutch

--- server-modules/nginx.nix
+++ server-modules/nginx.nix
@@ -14,6 +14,25 @@ in {
       forceSSL = true;
       locations."/" = { return = 444; };
     };
+    virtualHosts."api.${domain}" = {
+      useACMEHost = "api.${domain}";
+      forceSSL = true;
+      locations."/mc" = {
+        proxyPass =
+          "http://localhost:5000"; # Forward requests to backend on port 5000
+        proxySetHeader = {
+          "X-Real-IP" = "$remote_addr";
+          "X-Forwarded-For" = "$proxy_add_x_forwarded_for";
+          "X-Forwarded-Proto" = "$scheme";
+          "Host" = "$host";
+        };
+        addHeader = {
+          "Access-Control-Allow-Origin" = "*";
+          "Access-Control-Allow-Methods" = "GET, POST, OPTIONS";
+          "Access-Control-Allow-Headers" = "Authorization, Content-Type";
+        };
+      };
+    };
   };
 
   security.acme = {
