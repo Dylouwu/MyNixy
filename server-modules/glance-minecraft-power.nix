7f76e799e4c0807cb4829464bb85e1f63c0e82f6
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-04-12 11:31:02 +0200

    added ports cause lol

--- server-modules/glance-minecraft-power.nix
+++ server-modules/glance-minecraft-power.nix
@@ -2,6 +2,7 @@
   services.glance-minecraft-power = {
     enable = true;
     minecraftServerName = "paradisum";
+    host = "mc.dilou.me";
     apiKeyPath = config.sops.secrets.minecraft-api-key.path;
   };
 }
