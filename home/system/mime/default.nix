dab7f54981128f8a2ffad383a714fcc0db59e2f2
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-05-02 13:13:12 +0200

    mime update
    Former-commit-id: 7e6c3c3f58d576bfbf7f0d7a53d2bec6f25fe1c0 [formerly 58a3a8cda94d0f4e00eaca1aba60b53071a3a112]
    Former-commit-id: b6abf82977a3ae561cf58bf0981b60b7ce6b31cd

--- home/system/mime/default.nix
+++ home/system/mime/default.nix
@@ -1,50 +1,94 @@
-# Mime allows us to configure the default applications for each file type
-{
+{ pkgs, lib, ... }:
+with lib;
+let
+  defaultApps = {
+    browser = [ "zen.desktop" ];
+    text = [ "org.gnome.TextEditor.desktop" ];
+    image = [ "imv-dir.desktop" ];
+    audio = [ "mpv.desktop" ];
+    video = [ "mpv.desktop" ];
+    directory = [ "thunar.desktop" ];
+    office = [ "libreoffice.desktop" ];
+    pdf = [ "zathura.desktop" ];
+    terminal = [ "kitty.desktop" ];
+    discord = [ "discord.desktop" ];
+    archive = [ "xarchiver.desktop" ];
+  };
+
+  mimeMap = {
+    text = [ "text/plain" ];
+    image = [
+      "image/bmp"
+      "image/gif"
+      "image/jpeg"
+      "image/jpg"
+      "image/png"
+      "image/svg+xml"
+      "image/tiff"
+      "image/vnd.microsoft.icon"
+      "image/webp"
+    ];
+    audio = [
+      "audio/aac"
+      "audio/mpeg"
+      "audio/ogg"
+      "audio/opus"
+      "audio/wav"
+      "audio/webm"
+      "audio/x-matroska"
+    ];
+    video = [
+      "video/mp2t"
+      "video/mp4"
+      "video/mpeg"
+      "video/ogg"
+      "video/webm"
+      "video/x-flv"
+      "video/x-matroska"
+      "video/x-msvideo"
+    ];
+    directory = [ "inode/directory" ];
+    browser = [
+      "text/html"
+      "x-scheme-handler/about"
+      "x-scheme-handler/http"
+      "x-scheme-handler/https"
+      "x-scheme-handler/unknown"
+    ];
+    office = [
+      "application/vnd.oasis.opendocument.text"
+      "application/vnd.oasis.opendocument.spreadsheet"
+      "application/vnd.oasis.opendocument.presentation"
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+      "application/msword"
+      "application/vnd.ms-excel"
+      "application/vnd.ms-powerpoint"
+      "application/rtf"
+    ];
+    pdf = [ "application/pdf" ];
+    terminal = [ "terminal" ];
+    archive = [
+      "application/zip"
+      "application/rar"
+      "application/7z"
+      "application/*tar"
+    ];
+    discord = [ "x-scheme-handler/discord" ];
+  };
+
+  associations = with lists;
+    listToAttrs (flatten (mapAttrsToList
+      (key: map (type: attrsets.nameValuePair type defaultApps."${key}"))
+      mimeMap));
+in {
   xdg = {
-    enable = true;
-    mime.enable = true;
-    userDirs.enable = true;
-    portal.xdgOpenUsePortal = true;
-    configFile."mimeapps.list".force =
-      true; # don't error when mimeapps.list is replaced, it gets replaced often
+    configFile."mimeapps.list".force = true;
     mimeApps = {
       enable = true;
-      defaultApplications = {
-        "text/markdown" = "org.gnome.TextEditor.desktop";
-        "text/plain" = "org.gnome.TextEditor.desktop";
-        "text/x-shellscript" = "org.gnome.TextEditor.desktop";
-        "text/x-python" = "org.gnome.TextEditor.desktop";
-        "text/x-go" = "org.gnome.TextEditor.desktop";
-        "text/css" = "org.gnome.TextEditor.desktop";
-        "text/javascript" = "org.gnome.TextEditor.desktop";
-        "text/x-c" = "org.gnome.TextEditor.desktop";
-        "text/x-c++" = "org.gnome.TextEditor.desktop";
-        "text/x-java" = "org.gnome.TextEditor.desktop";
-        "text/x-rust" = "org.gnome.TextEditor.desktop";
-        "text/x-yaml" = "org.gnome.TextEditor.desktop";
-        "text/x-toml" = "org.gnome.TextEditor.desktop";
-        "text/x-dockerfile" = "org.gnome.TextEditor.desktop";
-        "text/x-xml" = "org.gnome.TextEditor.desktop";
-        "text/x-php" = "org.gnome.TextEditor.desktop";
-        "image/jpeg" = "imv.desktop";
-        "image/jpg" = "imv.desktop";
-        "image/webp" = "imv.desktop";
-        "image/gif" = "zen.desktop";
-        "x-scheme-handler/http" = "zen.desktop";
-        "x-scheme-handler/https" = "zen.desktop";
-        "text/html" = "zen.desktop";
-        "inode/directory" = "thunar.desktop";
-        "application/zip" = "xarchiver.desktop";
-        "application/pdf" = "zathura.desktop";
-        "image/png" = "imv-dir.desktop";
-        "x-scheme-handler/chrome" = "zen.desktop";
-        "application/x-extension-htm" = "zen.desktop";
-        "application/x-extension-html" = "zen.desktop";
-        "application/x-extension-shtml" = "zen.desktop";
-        "application/xhtml+xml" = "zen.desktop";
-        "application/x-extension-xhtml" = "zen.desktop";
-        "application/x-extension-xht" = "zen.desktop";
-      };
+      associations.added = associations;
+      defaultApplications = associations;
     };
   };
 }
