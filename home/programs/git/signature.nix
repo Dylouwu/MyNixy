b25987c9fdfdf7bc17c9b57596451c37e62d4998
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-05-06 18:42:18 +0200

    added a CHANGEME to signature.nix
    Former-commit-id: 51042b42fd379c76dfd96b842c0906b37ac615b7 [formerly 33e2464644909361059d422d3f53d083d720f701]
    Former-commit-id: f5aecf28d55e4667b43b15f033061af6824f3463

--- home/programs/git/signature.nix
+++ home/programs/git/signature.nix
@@ -1,6 +1,6 @@
 {
   home.file.".ssh/allowed_signers".text =
-    "* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHeSbtK0Ny/63DeXra409ruJ4us46I2LwwkTO17TGLbl";
+    "* ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHeSbtK0Ny/63DeXra409ruJ4us46I2LwwkTO17TGLbl"; # CHANGEME: Replace with your own public key
   programs.git.extraConfig = {
     commit.gpgsign = true;
     gpg.ssh.allowedSignersFile = "~/.ssh/allowed_signers";
