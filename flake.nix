dc9825f64c42d0c2c25f5ab5f37c3eda8aea488e
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-05-28 21:16:45 +0200

    nvm zen is still broken
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>
    
    Former-commit-id: 289e63441c43d3e8cfcc08b4b32b32a5ba8be26b [formerly d2e276dd1bb25162f20a45341dab97cf6436e63e]
    Former-commit-id: ab63df633cfd0cbc2e4b0d64172044c8fc6e4f2f

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     stylix.url = "github:danth/stylix";
     apple-fonts.url = "github:Lyndeno/apple-fonts.nix";
     zen-browser.url =
-      "github:0xc000022070/zen-browser-flake";
+      "github:0xc000022070/zen-browser-flake/3c4f98e9504d3f94bbd303d428162665a0ade8d6";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
