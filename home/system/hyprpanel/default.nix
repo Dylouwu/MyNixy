d9c73a2a855af936eef91998b727898f05cfacca
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-07-07 10:57:13 +0200

    update 2/3
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- home/system/hyprpanel/default.nix
+++ home/system/hyprpanel/default.nix
@@ -1,191 +1,224 @@
 # Hyprpanel is the bar on top of the screen
 # Display informations like workspaces, battery, wifi, ...
-{ inputs, config, ... }:
+{ config, ... }:
 let
   transparentButtons = config.theme.bar.transparentButtons;
-
   accent = "#${config.lib.stylix.colors.base0D}";
   accent-alt = "#${config.lib.stylix.colors.base03}";
   background = "#${config.lib.stylix.colors.base00}";
   background-alt = "#${config.lib.stylix.colors.base01}";
   foreground = "#${config.lib.stylix.colors.base05}";
+  foregroundOnWallpaper = "#${config.theme.textColorOnWallpaper}";
   font = "${config.stylix.fonts.serif.name}";
-  fontSize = "${toString config.stylix.fonts.sizes.desktop}";
-
+  fontSizeForHyprpanel = "${toString config.stylix.fonts.sizes.desktop}px";
   rounding = config.theme.rounding;
   border-size = config.theme.border-size;
-
   gaps-out = config.theme.gaps-out;
   gaps-in = config.theme.gaps-in;
 
   floating = config.theme.bar.floating;
   transparent = config.theme.bar.transparent;
-  position = config.theme.bar.position;
+  position = config.theme.bar.position; # "top" or "bottom"
 
+  notificationOpacity = 90;
   location = config.var.location;
   weather-key = "/home/${config.var.username}/.weather.json";
-in {
 
-  imports = [ inputs.hyprpanel.homeManagerModules.hyprpanel ];
+in {
+  wayland.windowManager.hyprland.settings.exec-once = [ "hyprpanel" ];
 
   programs.hyprpanel = {
     enable = true;
-    hyprland.enable = true;
-    overwrite.enable = true;
-    overlay.enable = true;
-    settings.layout = {
-      "bar.layouts" = {
-        "*" = {
-          "left" = [ "dashboard" "workspaces" "windowtitle" ];
-          "middle" = [ "media" "cava" ];
-          "right" =
-            [ "systray" "volume" "battery" "network" "clock" "notifications" ];
+
+    settings = {
+      layout = {
+        bar.layouts = {
+          "*" = {
+            "left" = [ "dashboard" "workspaces" "windowtitle" ];
+            "middle" = [ "media" "cava" ];
+            "right" = [ "systray" "volume" "bluetooth" "battery" "network" "clock" "notifications" ];
+          };
         };
       };
-    };
 
-    override = {
-      "bar.battery.hideLabelWhenFull" = true;
-      "bar.bluetooth.label" = false;
-      "bar.clock.format" = "%a %d %b  %H:%M";
-      "bar.customModules.cava.showActiveOnly" = true;
-      "bar.customModules.cava.showIcon" = false;
-      "bar.customModules.cava.stereo" = true;
-      "bar.customModules.updates.pollingInterval" = 1440000;
-      "bar.launcher.icon" = "";
-      "bar.media.show_active_only" = true;
-      "bar.network.truncation_size" = 12;
-      "bar.notifications.hideCountWhenZero" = true;
-      "bar.notifications.show_total" = true;
-      "bar.volume.label" = false;
-      "bar.windowtitle.label" = true;
-      "bar.workspaces.applicationIconEmptyWorkspace" = "";
-      "bar.workspaces.hideUnoccupied" = false;
-      "bar.workspaces.monitorSpecific" = false;
-      "bar.workspaces.numbered_active_indicator" = "color";
-      "bar.workspaces.show_numbered" = false;
-      "bar.workspaces.showApplicationIcons" = true;
-      "bar.workspaces.showWsIcons" = true;
-      "bar.workspaces.workspaces" = 1;
-      "menus.clock.weather.key" = "${weather-key}";
-      "menus.clock.weather.location" = "${location}";
-      "menus.clock.weather.unit" = "metric";
-      "menus.dashboard.directories.enabled" = false;
-      "menus.dashboard.powermenu.confirmation" = false;
-      "menus.dashboard.powermenu.avatar.image" = "~/.face.icon";
-      "menus.dashboard.shortcuts.left.shortcut1.command" = "zen";
-      "menus.dashboard.shortcuts.left.shortcut1.icon" = "";
-      "menus.dashboard.shortcuts.left.shortcut1.tooltip" = "Zen";
-      "menus.dashboard.shortcuts.left.shortcut2.command" = "caffeine";
-      "menus.dashboard.shortcuts.left.shortcut2.icon" = "󰅶";
-      "menus.dashboard.shortcuts.left.shortcut2.tooltip" = "Caffeine";
-      "menus.dashboard.shortcuts.left.shortcut3.command" = "night-shift";
-      "menus.dashboard.shortcuts.left.shortcut3.icon" = "󰖔";
-      "menus.dashboard.shortcuts.left.shortcut3.tooltip" = "Night-shift";
-      "menus.dashboard.shortcuts.left.shortcut4.command" = "menu";
-      "menus.dashboard.shortcuts.left.shortcut4.icon" = "";
-      "menus.dashboard.shortcuts.left.shortcut4.tooltip" = "Search Apps";
-      "menus.dashboard.shortcuts.right.shortcut1.command" = "hyprpicker -a";
-      "menus.dashboard.shortcuts.right.shortcut1.icon" = "";
-      "menus.dashboard.shortcuts.right.shortcut1.tooltip" = "Color Picker";
-      "menus.dashboard.shortcuts.right.shortcut3.command" =
-        "screenshot region swappy";
-      "menus.dashboard.shortcuts.right.shortcut3.icon" = "󰄀";
-      "menus.dashboard.shortcuts.right.shortcut3.tooltip" = "Screenshot";
-      "menus.dashboard.stats.enable_gpu" = false;
-      "notifications.active_monitor" = false;
-      "notifications.displayedTotal" = 5;
-      "scalingPriority" = "hyprland";
-      "theme.bar.background" = "${background
-        + (if transparentButtons && transparent then "00" else "")}";
-      "theme.bar.border_radius" = "${toString rounding}px";
-      "theme.bar.buttons.background" =
-        "${(if transparent then background else background-alt)
-        + (if transparentButtons then "00" else "")}";
-      "theme.bar.buttons.hover" = "${background}";
-      "theme.bar.buttons.icon" = "${accent}";
-      "theme.bar.buttons.monochrome" = true;
-      "theme.bar.buttons.notifications.background" = "${background-alt}";
-      "theme.bar.buttons.notifications.hover" = "${background}";
-      "theme.bar.buttons.notifications.icon" = "${accent}";
-      "theme.bar.buttons.notifications.total" = "${accent}";
-      "theme.bar.buttons.padding_x" = "0.8rem";
-      "theme.bar.buttons.padding_y" = "0.4rem";
-      "theme.bar.buttons.radius" = "${
-          if transparent then toString rounding else toString (rounding - 8)
-        }px";
-      "theme.bar.buttons.spacing" = "0.3em";
-      "theme.bar.buttons.style" = "default";
-      "theme.bar.buttons.text" = "${foreground}";
-      "theme.bar.buttons.workspaces.active" = "${accent}";
-      "theme.bar.buttons.workspaces.available" = "${accent-alt}";
-      "theme.bar.buttons.workspaces.hover" = "${accent-alt}";
-      "theme.bar.buttons.workspaces.occupied" = "${accent-alt}";
-      "theme.bar.buttons.y_margins" =
-        "${if floating && transparent then "0" else "8"}px";
-      "theme.bar.dropdownGap" = "4.5em";
-      "theme.bar.floating" = "${if floating then "true" else "false"}";
-      "theme.bar.location" = "${position}";
-      "theme.bar.margin_bottom" =
-        "${if position == "top" then "0" else toString (gaps-in * 2)}px";
-      "theme.bar.margin_sides" = "${toString gaps-out}px";
-      "theme.bar.margin_top" =
-        "${if position == "top" then toString (gaps-in * 2) else "0"}px";
-      "theme.bar.menus.background" = "${background}";
-      "theme.bar.menus.border.color" = "${accent}";
-      "theme.bar.menus.border.radius" = "${toString rounding}px";
-      "theme.bar.menus.border.size" = "${toString border-size}px";
-      "theme.bar.menus.buttons.active" = "${accent}";
-      "theme.bar.menus.buttons.default" = "${accent}";
-      "theme.bar.menus.card_radius" = "${toString rounding}px";
-      "theme.bar.menus.cards" = "${background-alt}";
-      "theme.bar.menus.check_radio_button.active" = "${accent}";
-      "theme.bar.menus.dropdownmenu.background" = "${background-alt}";
-      "theme.bar.menus.dropdownmenu.text" = "${foreground}";
-      "theme.bar.menus.iconbuttons.active" = "${accent}";
-      "theme.bar.menus.icons.active" = "${accent}";
-      "theme.bar.menus.label" = "${foreground}";
-      "theme.bar.menus.listitems.active" = "${accent}";
-      "theme.bar.menus.menu.media.background.color" = "${background-alt}";
-      "theme.bar.menus.menu.media.card.color" = "${background-alt}";
-      "theme.bar.menus.menu.media.card.tint" = 90;
-      "theme.bar.menus.monochrome" = true;
-      "theme.bar.menus.popover.background" = "${background-alt}";
-      "theme.bar.menus.popover.text" = "${foreground}";
-      "theme.bar.menus.progressbar.foreground" = "${accent}";
-      "theme.bar.menus.shadow" =
-        "${if transparent then "0 0 0 0" else "0px 0px 3px 1px #16161e"}";
-      "theme.bar.menus.slider.primary" = "${accent}";
-      "theme.bar.menus.switch.enabled" = "${accent}";
-      "theme.bar.menus.text" = "${foreground}";
-      "theme.bar.menus.tooltip.background" = "${background-alt}";
-      "theme.bar.menus.tooltip.text" = "${foreground}";
-      "theme.bar.outer_spacing" =
-        "${if floating && transparent then "0" else "8"}px";
-      "theme.bar.transparent" = "${if transparent then "true" else "false"}";
-      "theme.font.name" = "${font}";
-      "theme.font.size" = "${fontSize}px";
-      "theme.notification.actions.background" = "${accent}";
-      "theme.notification.actions.text" = "${foreground}";
-      "theme.notification.background" = "${background-alt}";
-      "theme.notification.border" = "${background-alt}";
-      "theme.notification.border_radius" = "${toString rounding}px";
-      "theme.notification.label" = "${accent}";
-      "theme.notification.labelicon" = "${accent}";
-      "theme.notification.text" = "${foreground}";
-      "theme.osd.bar_color" = "${accent}";
-      "theme.osd.bar_container" = "${background-alt}";
-      "theme.osd.bar_overflow_color" = "${accent-alt}";
-      "theme.osd.enable" = true;
-      "theme.osd.icon" = "${background}";
-      "theme.osd.icon_container" = "${accent}";
-      "theme.osd.label" = "${accent}";
-      "theme.osd.location" = "left";
-      "theme.osd.margins" = "0px 0px 0px 10px";
-      "theme.osd.muted_zero" = true;
-      "theme.osd.orientation" = "vertical";
-      "theme.osd.radius" = "${toString rounding}px";
-      "wallpaper.enable" = false;
+      theme.font.name = font;
+      theme.font.size = fontSizeForHyprpanel;
+      theme.bar = {
+        outer_spacing = if floating && transparent then "0px" else "8px";
+        floating = floating;
+        margin_top = (if position == "top" then toString (gaps-in * 2) else "0") + "px";
+        margin_bottom = (if position == "top" then "0" else toString (gaps-in * 2)) + "px";
+        margin_sides = toString gaps-out + "px";
+        border_radius = toString rounding + "px";
+        transparent = transparent;
+        location = position;
+        dropdownGap = "4.5em";
+        background = background + (if transparentButtons && transparent then "00" else "");
+      };
+
+      theme.bar.buttons = {
+        y_margins = if floating && transparent then "0px" else "8px";
+        spacing = "0.3em";
+        radius = (if transparent then toString rounding else toString (rounding - 8)) + "px";
+        padding_x = "0.8rem";
+        padding_y = "0.4rem";
+        style = "default";
+        monochrome = true;
+        text = if transparent && transparentButtons then foregroundOnWallpaper else foreground;
+        background = (if transparent then background else background-alt) + (if transparentButtons then "00" else "");
+        icon = accent;
+        hover = background;
+        workspaces = {
+          hover = accent-alt;
+          active = accent;
+          available = accent-alt;
+          occupied = accent-alt;
+        };
+        notifications = {
+          background = background-alt;
+          hover = background;
+          total = accent;
+          icon = accent;
+        };
+      };
+
+      theme.bar.menus = {
+        shadow = if transparent then "0 0 0 0" else "0px 0px 3px 1px #16161e";
+        monochrome = true;
+        card_radius = toString rounding + "px";
+        border = {
+          size = toString border-size + "px";
+          radius = toString rounding + "px";
+          color = accent;
+        };
+        menu.media = {
+          card.tint = 90;
+          background.color = background-alt;
+          card.color = background-alt;
+        };
+        background = background;
+        cards = background-alt;
+        label = foreground;
+        text = foreground;
+        popover = {
+          text = foreground;
+          background = background-alt;
+        };
+        listitems.active = accent;
+        icons.active = accent;
+        switch.enabled = accent;
+        check_radio_button.active = accent;
+        buttons = {
+          default = accent;
+          active = accent;
+        };
+        iconbuttons.active = accent;
+        progressbar.foreground = accent;
+        slider.primary = accent;
+        tooltip = {
+          background = background-alt;
+          text = foreground;
+        };
+        dropdownmenu = {
+          background = background-alt;
+          text = foreground;
+        };
+      };
+
+      bar = {
+        launcher.icon = "";
+        workspaces = {
+          show_numbered = false;
+          workspaces = 1;
+          numbered_active_indicator = "color";
+          monitorSpecific = false;
+          applicationIconEmptyWorkspace = "";
+          showApplicationIcons = true;
+          showWsIcons = true;
+        };
+        windowtitle.label = true;
+        volume.label = false;
+        network.truncation_size = 12;
+        bluetooth.label = false;
+        clock.format = "%a %b %d  %I:%M %p";
+        notifications.show_total = true;
+        media.show_active_only = true;
+        customModules = {
+          updates.pollingInterval = 1440000;
+          cava = {
+            showIcon = false;
+            stereo = true;
+            showActiveOnly = true;
+          };
+        };
+      };
+
+      notifications = {
+        position = "top right";
+        showActionsOnHover = true;
+      };
+      theme.notification = {
+        opacity = notificationOpacity;
+        enableShadow = true;
+        border_radius = toString rounding + "px";
+        background = background-alt;
+        actions = {
+          background = accent;
+          text = foreground;
+        };
+        label = accent;
+        border = background-alt;
+        text = foreground;
+        labelicon = accent;
+        close_button = {
+          background = background-alt;
+          label = "#f38ba8";
+        };
+      };
+
+      theme.osd = {
+        enable = true;
+        orientation = "vertical";
+        location = "left";
+        radius = toString rounding + "px";
+        margins = "0px 0px 0px 10px";
+        muted_zero = true;
+        bar_color = accent;
+        bar_overflow_color = accent-alt;
+        icon = background;
+        icon_container = accent;
+        label = accent;
+        bar_container = background-alt;
+      };
+
+      menus = {
+        clock.weather = {
+          location = location;
+          unit = "metric";
+          key = weather-key;
+        };
+        dashboard = {
+          powermenu = {
+            confirmation = false;
+            avatar.image = "~/.face.icon";
+          };
+          shortcuts = {
+            left = {
+              shortcut1 = { icon = ""; command = "zen"; tooltip = "Zen"; };
+              shortcut2 = { icon = "󰅶"; command = "caffeine"; tooltip = "Caffeine"; };
+              shortcut3 = { icon = "󰖔"; command = "night-shift"; tooltip = "Night-shift"; };
+              shortcut4 = { icon = ""; command = "menu"; tooltip = "Search Apps"; };
+            };
+            right = {
+              shortcut1 = { icon = ""; command = "hyprpicker -a"; tooltip = "Color Picker"; };
+              shortcut3 = { icon = "󰄀"; command = "screenshot region swappy"; tooltip = "Screenshot"; };
+            };
+          };
+        };
+        power.lowBatteryNotification = true;
+      };
+
+      wallpaper.enable = false;
     };
   };
 }
