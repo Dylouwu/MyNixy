70444a9e2d00d8d36f4d340c504133ab4ef40817
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-07-24 20:16:27 +0200

    powermode script
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

---
+++ home/scripts/powermode/default.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+let
+  powermode-toggle = pkgs.writeShellScriptBin "powermode-toggle" # bash
+    ''
+      current_profile=$(powerprofilesctl get)
+      if [ "$current_profile" = "performance" ]; then
+        powerprofilesctl set balanced
+        notif "powermode" "󰗑 Balanced Mode Activated" "Enjoy the balance!"
+      else
+        powerprofilesctl set performance
+        notif "powermode" "󱐋 Performance Mode Activated" "Enjoy the power!"
+      fi
+    '';
+in { home.packages = [ powermode-toggle ]; }
