cead035c5d3e12d950399ea23402fa428dd5e701
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-08-14 22:17:44 +0200

    holy fuck i might actually kill myself but this look decent, fuck declarative ricing
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- home/system/waybar/swaync.nix
+++ home/system/waybar/swaync.nix
@@ -12,7 +12,7 @@
       control-center-margin-bottom = 250;
       control-center-margin-right = 10;
 
-      notification-window-width = 400;
+      notification-window-width = 380;
       notification-icon-size = 48;
       notification-body-image-height = 80;
       notification-body-image-width = 160;
@@ -36,10 +36,6 @@
           button-text = "Clear All";
         };
         dnd = { text = "Do Not Disturb"; };
-        label = {
-          max-lines = 3;
-          text = "Label Text";
-        };
         mpris = {
           image-size = 64;
           blur = true;
@@ -47,11 +43,31 @@
       };
     };
     style = ''
-      * {
-        font-size: 14px;
+      .notification,
+      .notification.low,
+      .notification.normal,
+      .notification.critical,      
+      .notification-default-action,
+      .notification-default-action:hover,
+      .notification-default-action:active,
+      .notification-row:focus,
+      .notification-group:focus,
+      .notification-group.collapsed .notification-row .notification, 
+      .control-center .notification-row .notification-background,
+      .control-center .notification-row .notification-background:hover,
+      .control-center .notification-row .notification-background:active {
+        background: transparent;
+        border: none;
+        outline: none;
+        box-shadow: none; 
+        margin: 0;
+        padding: 0;
       }
 
       .control-center {
+        background: @base00;
+        border: 1px solid @base0D;
+        color: @base05;
         padding: 5px;
         border-radius: 15px;
       }
@@ -60,38 +76,32 @@
         margin: 4px 5px; 
       }
 
-
-      .notification-row {
-        background: transparent !important;
-        border: none !important;
-        outline: none !important;
-        margin: 8px !important;
-      }
-
-      .widget-notifications {
-        background: transparent !important;
-        border: none !important;
-        padding: 0 !important;
-        margin: 0 !important;
-      }
-
-      .notification {
-        margin: 0 !important; 
-        padding: 14px;
-        border-radius: 12px; 
-      }
-
       .notification-content {
-        margin-left: 10px;
+        border-radius: 12px;
+        padding: 10px;
+        margin: 8px;
       }
 
       .notification-title {
-        font-size: 14px;
         font-weight: bold;
+        color: @base05;
       }
 
-      .notification-body {
-        font-size: 13px;
+      .close-button {
+        margin: 6px;
+        padding: 3px;
+        border-radius: 100px;
+        background-color: transparent;
+        border: 1px solid transparent;
+      }
+
+      .close-button:hover {
+        background-color: @base0C; 
+      }
+
+      .close-button:active {
+        background-color: @base0C;
+        color: @base00;
       }
     '';
   };
