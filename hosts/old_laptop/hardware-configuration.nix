ecffc8f27b4b25b432f77ef3404d2672897378c2
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-08-29 09:35:38 +0200

    Change EFI mount point from '/boot/efi' to '/boot' for old_laptop

--- hosts/old_laptop/hardware-configuration.nix
+++ hosts/old_laptop/hardware-configuration.nix
@@ -18,7 +18,7 @@
       fsType = "ext4";
     };
 
-  fileSystems."/boot/efi" =
+  fileSystems."/boot" =
     { device = "/dev/disk/by-uuid/3A35-6AA4";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
