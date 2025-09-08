a6a2b5da23195373f7f937f6fb43c75495626a57
Purin <118902463+Dylouwu@users.noreply.github.com>
2025-09-08 16:54:18 +0200

    added bit
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- server-modules/minecraft.nix
+++ server-modules/minecraft.nix
@@ -8,6 +8,64 @@
     dataDir = "/var/lib/minecraft";
 
     servers = {
+      bit = {
+        autoStart = false;
+        enable = true;
+        enableReload = true;
+        jvmOpts =
+          "-Xms8192M -Xmx8192M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:SurvivorRatio=32 -XX:G1HeapWastePercent=5 -XX:MaxTenuringThreshold=1 -XX:+PerfDisableSharedMem -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:+UseNUMA -XX:-DontCompileHugeMethods -XX:MaxNodeLimit=240000 -XX:NodeLimitFudgeFactor=8000 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:NmethodSweepActivity=1 -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:AllocatePrefetchStyle=3 -XX:+AlwaysActAsServerClassMachine -XX:+UseTransparentHugePages -XX:LargePageSizeInBytes=2M -XX:+UseLargePages -XX:+EagerJVMCI -XX:+UseStringDeduplication -XX:+UseAES -XX:+UseAESIntrinsics -XX:+UseFMA -XX:+UseLoopPredicate -XX:+RangeCheckElimination -XX:+OptimizeStringConcat -XX:+UseCompressedOops -XX:+UseThreadPriorities -XX:+OmitStackTraceInFastThrow -XX:+RewriteBytecodes -XX:+RewriteFrequentPairs -XX:+UseFPUForSpilling -XX:+UseFastStosb -XX:+UseNewLongLShift -XX:+UseVectorCmov -XX:+UseXMMForArrayCopy -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+UseXmmLoadAndClearUpper -XX:+UseXmmRegToRegMoveAll -XX:+EliminateLocks -XX:+DoEscapeAnalysis -XX:+AlignVector -XX:+OptimizeFill -XX:+EnableVectorSupport -XX:+UseCharacterCompareIntrinsics -XX:+UseCopySignIntrinsic -XX:+UseVectorStubs -XX:UseAVX=2 -XX:UseSSE=4 -XX:+UseFastJNIAccessors -XX:+UseInlineCaches -XX:+SegmentedCodeCache";
+        package = pkgs.paperServers.paper-1_21_5-build_114;
+        restart = "no";
+
+        serverProperties = {
+          allow-nether = false;
+          difficulty = 0;
+          enable-command-block = true;
+          gamemode = "creative";
+          level-type = "minecraft\\:flat";
+          max-chained-neighbor-updates = 100000;
+          player-idle-timeout = 30;
+          server-port = 1337;
+          simulation-distance = 8;
+          spawn-protection = 0;
+          view-distance = 12;
+          white-list = true;
+        };
+
+        whitelist = {
+          Pur1rin = "97f095e9-0b9d-4435-a65c-2285461bacbe";
+          Azrabin = "901ea57e-865d-4a02-b5dc-0179fcc44881";
+        };
+
+        symlinks = {
+          "cache/mojang_1.21.5.jar" =
+            "${pkgs.vanillaServers.vanilla-1_21_5}/lib/minecraft/server.jar";
+          "plugins/worldedit.jar" = pkgs.fetchurl {
+            url =
+              "https://cdn.modrinth.com/data/1u6JkXh5/versions/Jk1z2u7n/worldedit-bukkit-7.3.16.jar";
+            hash = "sha256-EQdQDVe3qSHOmECQAzr8wqJyse1qAzap/X8x96y2Pw8=";
+          };
+        };
+
+        files = {
+          "server-icon.png" = ../src/blue.png;
+          "ops.json".value = [
+            {
+              name = "Pur1rin";
+              uuid = "97f095e9-0b9d-4435-a65c-2285461bacbe";
+              level = 4;
+              bypassesPlayerLimit = true;
+            }
+            {
+              name = "Azrabin";
+              uuid = "901ea57e-865d-4a02-b5dc-0179fcc44881";
+              level = 4;
+              bypassesPlayerLimit = true;
+            }
+          ];
+        };
+      };
+
       paradisum = {
         autoStart = false;
         enable = true;
