586f31ab478aeb6c7798a78356116f0da966b527
Purin <118902463+Dylouwu@users.noreply.github.com>
2026-01-18 22:53:20 +0100

    cobblemon init
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- server-modules/minecraft.nix
+++ server-modules/minecraft.nix
@@ -8,13 +8,13 @@
     dataDir = "/var/lib/minecraft";
 
     servers = {
-      multi = {
+      cobblemon = {
         autoStart = false;
         enable = true;
         enableReload = true;
         jvmOpts =
           "-Xms16384M -Xmx16384M -XX:+UseG1GC -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1MaxNewSizePercent=50 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=15 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=20 -XX:G1MixedGCLiveThresholdPercent=90 -XX:SurvivorRatio=32 -XX:G1HeapWastePercent=5 -XX:MaxTenuringThreshold=1 -XX:+PerfDisableSharedMem -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:+UseNUMA -XX:-DontCompileHugeMethods -XX:MaxNodeLimit=240000 -XX:NodeLimitFudgeFactor=8000 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:NmethodSweepActivity=1 -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:AllocatePrefetchStyle=3 -XX:+AlwaysActAsServerClassMachine -XX:+UseTransparentHugePages -XX:LargePageSizeInBytes=2M -XX:+UseLargePages -XX:+EagerJVMCI -XX:+UseStringDeduplication -XX:+UseAES -XX:+UseAESIntrinsics -XX:+UseFMA -XX:+UseLoopPredicate -XX:+RangeCheckElimination -XX:+OptimizeStringConcat -XX:+UseCompressedOops -XX:+UseThreadPriorities -XX:+OmitStackTraceInFastThrow -XX:+RewriteBytecodes -XX:+RewriteFrequentPairs -XX:+UseFPUForSpilling -XX:+UseFastStosb -XX:+UseNewLongLShift -XX:+UseVectorCmov -XX:+UseXMMForArrayCopy -XX:+UseXmmI2D -XX:+UseXmmI2F -XX:+UseXmmLoadAndClearUpper -XX:+UseXmmRegToRegMoveAll -XX:+EliminateLocks -XX:+DoEscapeAnalysis -XX:+AlignVector -XX:+OptimizeFill -XX:+EnableVectorSupport -XX:+UseCharacterCompareIntrinsics -XX:+UseCopySignIntrinsic -XX:+UseVectorStubs -XX:UseAVX=2 -XX:UseSSE=4 -XX:+UseFastJNIAccessors -XX:+UseInlineCaches -XX:+SegmentedCodeCache";
-        package = pkgs.paperServers.paper-1_21_10;
+        package = pkgs.fabricServers.fabric-1_21_1;
         restart = "no";
 
         serverProperties = {
@@ -40,8 +40,8 @@
         };
 
         symlinks = {
-          "cache/mojang_1.21.10.jar" =
-            "${pkgs.vanillaServers.vanilla-1_21_10}/lib/minecraft/server.jar";
+          "cache/mojang_1.21.1.jar" =
+            "${pkgs.vanillaServers.vanilla-1_21_1}/lib/minecraft/server.jar";
           # "plugins/worldedit.jar" = pkgs.fetchurl {
           #   url =
           #     "https://cdn.modrinth.com/data/1u6JkXh5/versions/Jk1z2u7n/worldedit-bukkit-7.3.16.jar";
@@ -50,7 +50,7 @@
         };
 
         files = {
-          "server-icon.png" = ../src/blue.png;
+          "server-icon.png" = ../src/cherry.png;
           "ops.json".value = [
             {
               name = "Pur1rin";
