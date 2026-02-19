6d5726620bc41d0e253c7ea53ee27f72b221fdd5
Purin <118902463+Dylouwu@users.noreply.github.com>
2026-02-20 00:51:35 +0100

    updated hyrule + changed minecraftservername for cobblemon
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- server-modules/glance-minecraft-power.nix
+++ server-modules/glance-minecraft-power.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
   services.glance-minecraft-power = {
     enable = true;
-    minecraftServerName = "paradisum";
+    minecraftServerName = "cobblemon";
     apiKeyPath = config.sops.secrets.minecraft-api-key.path;
   };
 }
