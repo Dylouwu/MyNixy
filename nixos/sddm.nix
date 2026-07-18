dab0c07acbe317a7dc3b8b86a83478417a03649b
Purin <118902463+Dylouwu@users.noreply.github.com>
2026-07-18 11:08:12 +0200

    qylock on mai
    Signed-off-by: Purin <118902463+Dylouwu@users.noreply.github.com>

--- nixos/sddm.nix
+++ nixos/sddm.nix
@@ -1,32 +1,47 @@
-{ pkgs, config, inputs, ... }:
+{ pkgs, inputs, ... }:
+
 let
-  sddm-astronaut = pkgs.sddm-astronaut.override {
-    embeddedTheme = "hyprland_kath";
-    themeConfig = {
-      ScreenWidth = config.var.monitors.monitor1.width;
-      ScreenHeight = config.var.monitors.monitor1.height;
-      HideVirtualKeyboard = true;
-      HideSystemButtons = false;
-      HideLoginButton = true;
+
+  qylock-theme = pkgs.stdenv.mkDerivation {
+    name = "qylock-theme";
+    src = pkgs.fetchFromGitHub {
+      owner = "Darkkal44";
+      repo = "qylock";
+      rev = "3ecb79f621d5bfc2fbc6bfd37c3b12f0214601ac";
+      hash = "sha256-NfCuiVlJsXp3k1nK9XieKdL0v/pbiMKfyU5A3Gc3fpk=";
     };
+
+    installPhase = ''
+      mkdir -p $out/share/sddm/themes/girl-pillow
+      cp -r themes/girl-pillow/. $out/share/sddm/themes/girl-pillow
+    '';
   };
-in {
+in
+{
   services.displayManager = {
     sddm = {
-      package = pkgs.kdePackages.sddm;
-      extraPackages = [ sddm-astronaut ];
       enable = true;
       wayland.enable = true;
-      theme = "sddm-astronaut-theme";
+      package = pkgs.kdePackages.sddm;
+
+      theme = "girl-pillow";
+
+      extraPackages = with pkgs; [
+        qylock-theme
+        kdePackages.qtdeclarative
+        kdePackages.qt5compat
+        kdePackages.qtmultimedia
+      ];
+
       settings = {
         Wayland.SessionDir = "${
-            inputs.hyprland.packages."${pkgs.stdenv.hostPlatform.system}".hyprland
-          }/share/wayland-sessions";
+          inputs.hyprland.packages."${pkgs.stdenv.hostPlatform.system}".hyprland
+        }/share/wayland-sessions";
       };
     };
   };
 
-  environment.systemPackages = [ sddm-astronaut ];
+  environment.systemPackages = [ qylock-theme ];
 
   systemd.settings.Manager.DefaultTimeoutStopSec = "10s";
 }
